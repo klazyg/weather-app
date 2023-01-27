463e63e1b30fb3561611c20f3ff47eb8007dbe08
klazyg <kzygmunt0@gmail.com>
2023-01-27 12:40:29 +0100

    add: props

--- components/Currentdata/Currentdata.tsx
+++ components/Currentdata/Currentdata.tsx
@@ -1,7 +1,15 @@
-import React from "react";
 import styles from "./Currentdata.module.scss";
 
-function Currentdata({data}) {
+type CurrentdataProps = {
+  data: {
+    name: string;
+    sys: {
+      country: string;
+    }
+  }
+}
+
+const Currentdata: React.FC<CurrentdataProps> = ({ data }) => {
   return (
     <div className={styles.section}>
       <div className={styles.location}>
