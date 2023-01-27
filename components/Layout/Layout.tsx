b30ccc56cc6b6d9909a3b5b288834a9fdeb4039c
klazyg <kzygmunt0@gmail.com>
2023-01-27 12:38:18 +0100

    correct: layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import styles from "./Layout.module.scss";
 
-export default function Layout({ children }) {
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
   return (
-    <>
-      <div className={styles.inner}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </>
-  )
+    <div className={styles.inner}>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,20 @@
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Layout from '../components/Layout/Layout'
+import Head from 'next/head';
 import '../styles/globals.scss';
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-
-  const getLayout = Component.getLayout ?? ((page) => page)
-
-  return getLayout(<Component {...pageProps} />)
-}
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Weather App</title>
+        <meta name="description" content="Weather App created by Klaudia Zygmunt" />
+        <meta name="keywords" content="WEATHER APP PROJECT" />
+        <link rel="icon" href="/logo_icon.png" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
