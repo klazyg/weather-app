8b94b1254ce3937dca2a14b233cc52ae7ce79635
klazyg <kzygmunt0@gmail.com>
2023-01-27 12:38:44 +0100

    correct: errors & add: API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
-import Layout from '../components/Layout/Layout';
-import Buttons from '../components/Buttons/Buttons';
+import Button from '../components/Button/Button';
 import Input from '../components/Input/Input';
 import Currentdata from '../components/Currentdata/Currentdata';
 import Temperature from '../components/Temperature/Temperature';
 import axios from "axios";
 
 const BASE_URL = "http://api.openweathermap.org/data/2.5/weather/?q=";
+const API_KEY = "&units=metric&appid=ac5e8daa8e2f74ada85d8f25f774207e";
 
-//DELETED API
-const API_KEY = "&units=metric&appid=";
-
-function Weather() {
+const Weather: React.FC = () => {
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [city, setCity] = useState('Las Palmas');
+  const [city, setCity] = useState<string>('Las Palmas');
 
   const getWeather = async (city: string) => {
     try {
@@ -23,7 +20,7 @@ function Weather() {
       setWeather(response.data);
       setError(null);
     } catch (err) {
-      setError(err.message);
+      JSON.stringify(err);
       setWeather(null);
     } finally {
       setLoading(false);
@@ -34,26 +31,25 @@ function Weather() {
     getWeather(city);
   }, []);
 
-
   useEffect(() => {
-    getWeather(city);
+    if (city) {
+      getWeather(city);
+    }
   }, [city]);
 
   return (
-    <Layout>
-      <Buttons setCity={setCity} />
+    <>
+      <Button setCity={setCity} />
       <Input setCity={setCity} />
       {loading && <div>A moment please...</div>}
-      {error && (
-        <div>{`There is a problem fetching the post weather - ${error}`}</div>
-      )}
+      {error && <div>There is a problem fetching the post weather - ${error}</div>}
       {weather && (
         <div>
-          <Currentdata data={{ ...weather }} />
-          <Temperature data={{ ...weather }} />
+          <Currentdata data={weather} />
+          <Temperature data={weather} />
         </div>
-      )};
-    </Layout>
+      )}
+    </>
   )
 };
 
