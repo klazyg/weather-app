f5f406877cce36a2ad7aaf04ffb0ede4e88123f6
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-03-07 12:27:14 +0100

    add: mobile icon and correct styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,10 +6,18 @@ const Header: React.FC = () => {
     <header className={styles.position}>
       <div className={styles.logo}>
         <Image
+          className={styles.icon}
           src="/logo_icon.png"
           alt="Logo Icon"
           width={50}
           height={50}
+        />
+        <Image
+          className={styles.icon_mobile}
+          src="/logo_icon.png"
+          alt="Logo Icon"
+          width={33}
+          height={33}
         />
         <h1 className={styles.text}>Weather App</h1>
       </div>
